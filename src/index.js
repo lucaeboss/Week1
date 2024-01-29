53714ee0a4198e4fb17a513daa7b6c7efdb0954a
Luca1 <luca.caramihai@gmail.com>
2024-01-28 18:28:28 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-console.log('Hello World');
-for(i=1; i<=100;i++)
-{
-    if(i%2===0)
-    {
-        console.log(`even: ${i}`);
-    }
-    else
-    {
-        console.log(`odd: ${i}`);
-    }
-}
+const _ = require('lodash');
 
-let _=require('lodash');
+const numbers = [1, 2, 3, 4, 5, 6];
+const chunks = _.chunk(numbers, 2); 
+console.log(chunks); 
 
-console.log(_.camelCase('order number'))
+const phrase = "hello world";
+const camelCased = _.camelCase(phrase);
+console.log(camelCased); 
+
+const objects = [
+    { name: "Alice", age: 25 },
+    { name: "Bob", age: 30 },
+    { name: "Charlie", age: 20 }
+];
+const filtered = _.filter(objects, { age: 25 }); 
+console.log(filtered); 
+
+const numbers2 = [5, 2, 8, 1];
+const minNumber = _.min(numbers2);
+console.log(minNumber);
+
+const names = ["John", "Jane", "Peter"];
+const greetedNames = _.map(names, name => `Hello, ${name}!`);
+console.log(greetedNames);
